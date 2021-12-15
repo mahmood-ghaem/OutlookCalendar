5c8efba7f8b8b0b34d88e9fb591eda3adae89076
mahmood-ghaem <pejvakco@gmail.com>
2021-12-15 16:51:19 +0100

    Add project files.

---
+++ OutlookCalendar/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OutlookCalendar.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OutlookCalendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public ActionResult OauthRedirect()
+        {
+            var redirectUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?" +
+                              "&scope=Calendars.ReadWrite offline_access User.Read" +
+                              "&response_type=code" +
+                              "&response_mode=query" +
+                              "&state=de-medewerker" +
+                              "&redirect_uri=https://localhost:44337/oauth/callback" +
+                              "&client_id=MyClientID";
+            return Redirect(redirectUrl);
+        }
+    }
+}
---
+++ OutlookCalendar/Controllers/OAuthController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System;
+
+namespace OutlookCalendar.Controllers
+{
+    public class OAuthController : Controller
+    {
+        string tokensFile = "D:\\tokens.json";
+        public ActionResult Callback(string code, string state, string error)
+        {
+            if (!string.IsNullOrWhiteSpace(code))
+            {
+                RestClient restClient = new RestClient();
+                RestRequest restRequest = new RestRequest();
+
+                restRequest.AddParameter("client_id", "53631b02-a068-450b-ae24-16145bf087e0");
+                restRequest.AddParameter("scope", "Calendars.ReadWrite offline_access User.Read");
+                restRequest.AddParameter("redirect_uri", "https://localhost:44337/oauth/callback");
+                restRequest.AddParameter("code", code);
+                restRequest.AddParameter("grant_type", "authorization_code");
+                restRequest.AddParameter("client_secret", "MyClientSecret");
+
+                restClient.BaseUrl = new Uri("https://login.microsoftonline.com/common/oauth2/v2.0/token");
+                var response = restClient.Post(restRequest);
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    System.IO.File.WriteAllText(tokensFile, response.Content);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return RedirectToAction("Error", "Home");
+        }
+    }
+}
